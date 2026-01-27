106fcd9e66d34b925338655ee1b5845b524ab891
Paul Moore <mrwog308@gmail.com>
2026-01-26 18:55:27 -0800

    changed textContent update after reviewing peer code

--- w3/app.js
+++ w3/app.js
@@ -13,8 +13,7 @@ const handleOrderSubmit = function(event) {
     event.preventDefault();
     const formData = orderForm.getOrderInputs();
     totalShirts += formData.qty;
-    let message = `Ordered ${formData.qty} ${formData.size} T-Shirt(s)${formData.giftWrap ? ' with' : ' without'} gift wrap.`;
-    orderSummary.textContent = message;
+    orderSummary.textContent = `You ordered ${formData.qty} ${formData.size} T-Shirt(s)${formData.giftWrap ? ' with' : ' without'} gift wrap.`;
 };
 
 const init = function() {
