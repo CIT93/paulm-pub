75a7af3fcb09257b587221be22731f9da343198e
Paul Moore <mrwog308@gmail.com>
2026-01-29 23:05:34 -0800

    Step 3 Complete

--- w4/app.js
+++ w4/app.js
@@ -3,15 +3,30 @@ console.log('Hello from app.js! Your JavaScript is connected and running!');
 // We use document.getElementById() to get a reference to an element by its unique ID.
 
 import * as orderForm from './order-handler.js';
+import * as priceCalculator from './price-calculator.js';
 
 const orderFormElement = document.getElementById('order-form');
 const orderSummary = document.getElementById('order-summary');
+
+const orders = [];
 
 let totalShirts = 1;
 
 const handleOrderSubmit = function(event) {
     event.preventDefault();
     const formData = orderForm.getOrderInputs();
+    const calcData = priceCalculator.calculateTotal(formData);
+    // console.log('Total calculated: ', calcData);
+
+    const newOrder = {
+        ...formData,
+        ...calcData,
+        timestamp: new Date().toISOString()
+    };
+
+    orders.push(newOrder);
+    console.log(orders);
+    
     totalShirts += formData.qty;
     orderSummary.textContent = `You ordered ${formData.qty} ${formData.size} T-Shirt(s)${formData.giftWrap ? ' with' : ' without'} gift wrap.`;
 };
