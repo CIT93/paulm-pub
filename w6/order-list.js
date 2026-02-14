471eec5aafed344802194f63e7b8bdd084f4f8c4
Paul Moore <mrwog308@gmail.com>
2026-02-13 19:17:54 -0800

    Step 2 Complete

---
+++ w6/order-list.js
@@ -0,0 +1,43 @@
+
+const orderTable = document.getElementById('order-talble');
+const orderList = orderTable.querySelector('order-table-body');
+const noEntriesMessage = document.getElementById('noEntriesMessage');
+
+const formatDateForDisplay = function(timestamp){
+    const date = new Date(timestamp);
+    return date.toLocaleDateString('en-US',{
+        year: 'numeric', month: 'short', day: 'numeric'
+    });
+};
+
+const createTableRow = function(entry){
+    const row = document.createElement('tr');
+    row.dataset.id = entry.id;
+    row.innerHTML = `
+        <td>${formatDateForDisplay(entry.timestamp)}</td>
+        <td>${entry.qty}</td>
+        <td>${entry.size}</td>
+        <td>${entry.totalPrice}</td>
+        `;
+}
+
+export const renderOrders = function(orders){
+    orderList.innerHTML = '';
+    if (orders.length === 0){
+        orderTable.style.display = 'none';
+        noEntriesMessage.style.display = 'block';
+        return;
+    }else{
+        orderTable.style.display = 'table';
+        noEntriesMessage.style.display = 'none';
+    };
+
+    const sortedOrders = [...orders].sort(function(a,b){
+        return new Date(b.timestamp) - new Date(a.timestamp)
+    });
+
+    for (const order of sortedOrders){
+        const rowElement = createTableRow(order);
+        orderList.appendChild(rowElement);
+    };
+};
